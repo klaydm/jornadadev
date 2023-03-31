a2833a216e1d6ecadb072cf385a1a1500dd68e93
klaydm <klaydm@gmail.com>
2023-03-30 22:07:10 -0300

    Feat: Adicionada animação do footer

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react'
+import VideoFooter from "./components/footer/VideoFooter"
 import './video.css'
+
 
 function Video () {
   const videoRef = useRef(null)
@@ -24,6 +26,8 @@ function Video () {
         loop
         src='https://firebasestorage.googleapis.com/v0/b/video-2db61.appspot.com/o/X2Download.app-%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD30%20Segundos%20status%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%EF%BF%BD%23Shitpost%20%23Statusmemes%20%2330segundosparastatus%20%23Shitpostraiz%20%23memes(720p).mp4?alt=media&token=3aad9e8d-b49a-4ec0-bfa1-57849e1cca79'
       ></video>
+      { /* Side bar */}
+      <VideoFooter />
     </div>
   )
 }
